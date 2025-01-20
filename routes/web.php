327c305f1f8691f3bdf31103b94ceb01be74f512
Farhan <syauqifarhan211@gmail.com>
2025-01-20 16:10:23 +0700

    b

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
 
-Route::get('/user/create', [UserController::class, 'create']);
-Route::post('/user/store', [UserController::class, 'store']);
+Route::get('/', function () {
+    return view('welcome');
+});
+
