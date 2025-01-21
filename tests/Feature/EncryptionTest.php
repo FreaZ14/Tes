854391613283e0c8574b795c3e87ab037c49915a
Farhan <syauqifarhan211@gmail.com>
2025-01-21 16:47:04 +0700

    e

---
+++ tests/Feature/EncryptionTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class EncryptionTest extends TestCase
+{
+    public function testEncryption(): void
+    {
+       $encrypt = Crypt::encrypt(value: 'Farhan Assyauqi');
+       var_dump(value:$encrypt);
+
+       $decrypt = Crypt::decrypt(payload: $encrypt);
+
+       self::assertEquals('Farhan Assyauqi', actual: $decrypt);
+    }
+}
