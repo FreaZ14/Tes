871b5274bb7cd8dcb8d052ebceef499ec3a1d92c
Farhan <syauqifarhan211@gmail.com>
2025-01-23 14:00:17 +0700

    rdr

---
+++ config/contoh.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    "author" => [
+        "first" => "Muhammad",
+        "last" => "Farhan Assyauqi"
+    ],
+    "email" => "syauqifarhan711@gmail.com",
+    "web" => "https://github.com/FreaZ14"
+];
