bf9cc518d3be39c2306daccf41ecc78f260e7817
Farhan <syauqifarhan211@gmail.com>
2025-01-24 13:24:37 +0700

    sc

--- app/Http/Controllers/InputController.php
+++ app/Http/Controllers/InputController.php
@@ -6,9 +6,8 @@ use Illuminate\Http\Request;
 
 class InputController extends Controller
 {
-    public function hello(Request $request): string
-    {
+    public function hello(Request $request): string {
         $name = $request->input('name');
-        return "Hello " . $name;
+        return "Hello $name";
     }
 }
