19ea1492a20103dc738b4110e7a8666e5ed6246e
Farhan <syauqifarhan211@gmail.com>
2025-01-24 10:47:29 +0700

    sc

--- tests/Feature/ServiceContainerTest.php
+++ tests/Feature/ServiceContainerTest.php
@@ -5,6 +5,8 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Data\Foo;
+use App\Data\Bar;
 
 class ServiceContainerTest extends TestCase
 {
