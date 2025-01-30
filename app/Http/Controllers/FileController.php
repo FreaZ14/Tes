f6196ff2eb79676c2559db5a34320134c5ae4502
Farhan <syauqifarhan211@gmail.com>
2025-01-30 11:13:31 +0700

    fU

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -6,5 +6,12 @@ use Illuminate\Http\Request;
 
 class FileController extends Controller
 {
-    //
+    public function upload(Request $request): string
+    {
+        $picture = $request->file('picture');
+
+        $picture->storePubliclyAs("pictures", $picture->getClientOriginalName(), "public");
+
+        return "OK $picture->getClientOriginalName()";
+}
 }
