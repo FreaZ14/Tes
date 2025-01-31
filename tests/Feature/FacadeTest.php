2fb66630c0769351c59230cd10f4774c19a00a3e
Farhan <syauqifarhan211@gmail.com>
2025-01-31 10:29:56 +0700

    fcd

---
+++ tests/Feature/FacadeTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class FacadeTest extends TestCase
+{
+    public function testConfig()
+    {
+        $firstname1 = config('contoh.author.first');
+        $firstname2 = Config::get('contoh.author.first');
+
+        self::assertEquals($firstname1, $firstname2);
+
+        var_dump(Config::all());
+    }
+}
