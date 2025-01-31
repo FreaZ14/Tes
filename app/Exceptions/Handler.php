a287ac8004b83d59d45ab82bf33ff64d1347524a
Farhan <syauqifarhan211@gmail.com>
2025-01-31 14:33:15 +0700

    eh

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -44,7 +44,7 @@ class Handler extends ExceptionHandler
     public function register()
     {
         $this->reportable(function (Throwable $e) {
-            var_dump($e);
+            //var_dump($e);
             return false;
         });
         $this->reportable(function (Throwable $e) {
