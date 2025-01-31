f5bfedab7ef2a7afe7f52deb3676736c8b617cb0
Farhan <syauqifarhan211@gmail.com>
2025-01-31 16:00:45 +0700

    sp

--- app/Providers/FooBarServiceProvider.php
+++ app/Providers/FooBarServiceProvider.php
@@ -1,28 +1,30 @@
 <?php
 
 namespace App\Providers;
-use App\Data\Foo;
+
 use App\Data\Bar;
+use App\Data\Foo;
 use App\Services\HelloService;
 use App\Services\HelloServiceIndonesia;
-use Illuminate\Contracts\Support\DeferableProvider;
+use Illuminate\Contracts\Support\DeferrableProvider;
 use Illuminate\Support\ServiceProvider;
-class FooBarServiceProvider extends ServiceProvider implements DeferableProvider
+
+class FooBarServiceProvider extends ServiceProvider implements DeferrableProvider
 {
     public array $singletons = [
-        HelloService::class => HelloServiceIndonesia::class
+        HelloService::class => HelloServiceIndonesia::class,
     ];
+
     public function register()
     {
         $this->app->singleton(Foo::class, function ($app) {
             return new Foo();
         });
-    
+
         $this->app->singleton(Bar::class, function ($app) {
             return new Bar($app->make(Foo::class));
         });
     }
-    
 
     /**
      * Bootstrap services.
@@ -39,5 +41,4 @@ class FooBarServiceProvider extends ServiceProvider implements DeferableProvider
         return [HelloService::class, Foo::class, Bar::class];
     }
 }
-
 
